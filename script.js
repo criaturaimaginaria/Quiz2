9be99b8621c18ee03b371964b63c03b25e6c3ee3
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-08-19 13:00:24 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let Quest = [];  //set an array for all answers
 
 const point = document.querySelector(".point")
+const point2 = document.querySelector(".point2")
+const point2bis = document.querySelector(".point2bis")
+const point3 = document.querySelector(".point3")
+const point3bis = document.querySelector(".point3bis")
 
 // this function gives each of the answers 0 points so if someone doesn't answer a question 
 // the whole thing will continue to work 
@@ -22,39 +26,124 @@ function saver(q, econPoints, poliPoints, socialPoints) {
   } 
   
 // this function uses the total calculated score to figure out which political ideology do you have
-function analyzer (myScore, VeScore) { 
-
-  if (myScore>34) {
-       arrContent = 5;
-     } //6
-    else  if (myScore > 20) { 
-        arrContent = 4;
-     } //5
-    else  if(myScore > 15)  {
-        arrContent = 3; 
-        point.style.cssText = `background-color: red; font-size: 1em; margin-left:${myScore}em; 
-        border-radius: 10px;` ;
-    } //4
-    else  if(myScore >= 6 ) {
-        arrContent = 2; 
-        point.style.cssText = `background-color: red; font-size: 1em; margin-left:${myScore}em;
-         border-radius: 10px;` ;
-    } //3
-    else if(myScore >= 4)     {
-        arrContent = 1; 
-        point.style.cssText = `background-color: red; font-size: 1em; margin-left:${myScore}em; 
-        border-radius: 10px;` ;
-    } //2
-    else  { arrContent = 4; 
-      point.style.cssText = `background-color: red; font-size: 1em; margin-left:${myScore}em;
-       border-radius: 10px; `;
-    } //1
+function analyzer (myScore, VeScore, socScore) { 
+  arrContent = 0
+// if for social score conservative 
+    if(socScore <= 4){
+          if (myScore & VeScore  > 20) { 
+            arrContent = 4;
+        } //5
+        else if(myScore & VeScore > 15)  {
+            arrContent = 3; 
+            point.style.cssText = `background-color: red; font-size: 1em; 
+            margin-left:${myScore}em; margin-top: ${VeScore}em; border-radius: 10px;` ;
+        } //4
+        else  if(myScore & VeScore >= 6 ) {
+            arrContent = 2; 
+            point.style.cssText = `background-color: red; font-size: 1em; 
+            margin-left:${myScore}em; margin-top: ${VeScore}em; border-radius: 10px;` ;
+        } //3
+        else if(myScore & VeScore >= 4)     {
+            arrContent = 1; 
+            point.style.cssText = `background-color: red; font-size: 1em; 
+            margin-left:${myScore}em; margin-top: ${VeScore}em; border-radius: 10px;` ;
+        } //2
+        else  { arrContent = 4; 
+          point.style.cssText = `background-color: red; font-size: 1em; 
+          margin-left:${myScore}em; margin-top: ${VeScore}em; border-radius: 10px; `;
+          
+        } //1
+       
+    }
     
-  myDisplay(myContents[arrContent]) 
+    console.log(arrContent)
+  // myDisplay(myContents[arrContent]) 
 
   // console.log(myScore)
-  // console.log(VeScore)
+  console.log(VeScore)
 } 
+
+function analyzer2 (myScore, VeScore, socScore) { 
+  arrContent = 0
+  // if for social score middle, not progresist and not conservative 
+      if(socScore > 6){
+            if (myScore & VeScore  > 20) { 
+              arrContent = 4;
+          } //5
+          else if(myScore & VeScore > 15)  {
+              arrContent = 3; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = ` font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //4
+          else  if(myScore & VeScore >= 6 ) {
+              arrContent = 2; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = `font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //3
+          else if(myScore & VeScore >= 4)     {
+              arrContent = 1; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = ` font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //2
+          else  { arrContent = 4; 
+            point3.style.cssText = `background-color: red;`
+            point3bis.style.cssText = ` font-size: 1em; 
+            margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px; `;
+            
+          } //1
+         
+      }
+      
+      console.log(arrContent)
+    // myDisplay(myContents[arrContent]) 
+  
+    // console.log(myScore)
+    console.log(VeScore)
+  } 
+
+function analyzer3 (myScore, VeScore, socScore) { 
+  arrContent = 0
+  // if for social score progresist 
+      if(socScore > 6){
+            if (myScore & VeScore  > 20) { 
+              arrContent = 4;
+          } //5
+          else if(myScore & VeScore > 15)  {
+              arrContent = 3; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = ` font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //4
+          else  if(myScore & VeScore >= 6 ) {
+              arrContent = 2; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = `font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //3
+          else if(myScore & VeScore >= 4)     {
+              arrContent = 1; 
+              point3.style.cssText = `background-color: red;`
+              point3bis.style.cssText = ` font-size: 1em; 
+              margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px;` ;
+          } //2
+          else  { arrContent = 4; 
+            point3.style.cssText = `background-color: red;`
+            point3bis.style.cssText = ` font-size: 1em; 
+            margin-left:${myScore}em; padding-top: ${VeScore}em; border-radius: 10px; `;
+            
+          } //1
+         
+      }
+      
+      console.log(arrContent)
+    // myDisplay(myContents[arrContent]) 
+  
+    // console.log(myScore)
+    console.log(VeScore)
+  } 
 
 
  // this function adds the number of points each answer is worth together 
@@ -74,7 +163,9 @@ function total() {
         } 
 
         // console.log(VeScore, VeScore, socScore);
-      analyzer(myScore, VeScore, socScore);       
+      analyzer(myScore, VeScore, socScore);      
+      analyzer2(myScore, VeScore, socScore);   
+      analyzer3(myScore, VeScore, socScore);    
   } 
 
 
