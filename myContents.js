10388c6f1836a0b00c401e8b0f7d6445c768bb86
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2021-08-21 16:28:55 -0300

    json, another js file etc

---
+++ myContents.js
@@ -0,0 +1,55 @@
+let conservador =  {
+   
+        0: {
+          name : "comunista",
+          description: "los comunistas son"
+        },
+
+        1: {
+          name: "socialista",
+          description: "fddddddddddddddddddddddddddddddddlpfldps"
+        },
+
+        2: {
+          name: "centro",
+          description: "los de scentro no tienen una ideología bla bla"
+        }
+}
+
+
+socialMiddle =  {
+   
+    0: {
+      name : "marxismo clasico",
+      description: "los comunistas son"
+    },
+
+    1: {
+      name: "cap iliberal",
+      description: "fddddddddddddddddddddddddddddddddlpfldps"
+    },
+
+    2: {
+      name: "clasic liberalismo",
+      description: "los de scentro no tienen una ideología bla bla"
+    }
+}
+
+progresista =  {
+   
+    0: {
+      name : "marxismo leninismo",
+      description: "los comunistas son"
+    },
+
+    1: {
+      name: "socioliberalismo",
+      description: "fddddddddddddddddddddddddddddddddlpfldps"
+    },
+
+    2: {
+      name: "agorismo",
+      description: "los de scentro no tienen una ideología bla bla"
+    }
+}
+
--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let Quest = [];  //set an array for all answers
 
+
 const consPoint = document.querySelector(".consPoint")
 const cons = document.querySelector(".cons")
 const midPoint = document.querySelector(".midPoint")
 const mid = document.querySelector(".mid")
 const progPoint = document.querySelector(".progPoint")
 const prog = document.querySelector(".prog")
+
+
+// fetch("./data.json")
+// .then(response => { return response.json();})
+// .then(data => console.log(data));
+
+
 
 // this function gives each of the answers 0 points so if someone doesn't answer a question 
 // the whole thing will continue to work 
@@ -15,7 +23,8 @@ function populate() {
     } 
 } 
 
-myContents =  ["comunista"  , "centrista"  , "capitalista", "ancap" , ]
+
+
 
 
 // each answer points into the array
@@ -27,115 +36,33 @@ function saver(q, econPoints, poliPoints, socialPoints) {
   
 // this function uses the total calculated score to figure out which political ideology do you have
 function analyzer (myScore, VeScore, socScore) { 
-  arrContent = 0
-// if for social score conservative 
+ 
+    // if for social score conservative 
     if(socScore <= 6.6){
-          if (myScore & VeScore  > 20) { 
-            arrContent = 4;
-        } //5
-        else if(myScore & VeScore > 15)  {
-            arrContent = 3; 
-            consPoint.style.cssText = `background-color: white;border-radius: 10px;`
-            cons.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-        } //4
-        else  if(myScore & VeScore >= 6 ) {
-            arrContent = 2; 
-            consPoint.style.cssText = `background-color: white;border-radius: 10px;`
-            cons.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-        } //3
-        else if(myScore & VeScore >= 4)     {
-            arrContent = 1; 
-            consPoint.style.cssText = `background-color: white;border-radius: 10px;`
-            cons.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-        } //2
-        else  { arrContent = 4; 
-          consPoint.style.cssText = `background-color: white;border-radius: 10px;`
-          cons.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-          
-        } //1
+      consPoint.style.cssText = `background-color: white;border-radius: 10px;`
+      cons.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
        
     }
-    
-    console.log(arrContent)
-  // myDisplay(myContents[arrContent]) 
 
-  // console.log(myScore)
-  console.log(VeScore)
+     // if for social score middle, not progresist and not conservative 
+     else if(socScore > 6.6 && socScore < 13){
+      midPoint.style.cssText = `background-color: white;border-radius: 10px;`
+      mid.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
+       
+    }
+
+      // if for social score progresist 
+      else if(socScore > 13){
+         
+        progPoint.style.cssText = `background-color: white;border-radius: 10px;`
+        prog.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
+         
+      }
+
+
 } 
 
-function analyzer2 (myScore, VeScore, socScore) { 
-  arrContent = 0
-  // if for social score middle, not progresist and not conservative 
-      if(socScore > 6.6 && socScore < 13){
-            if (myScore & VeScore  > 20) { 
-              arrContent = 4;
-          } //5
-          else if(myScore & VeScore > 15)  {
-              arrContent = 3; 
-              midPoint.style.cssText = `background-color: white;border-radius: 10px;`
-              mid.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-          } //4
-          else  if(myScore & VeScore >= 6 ) {
-              arrContent = 2; 
-              midPoint.style.cssText = `background-color: white;border-radius: 10px;`
-              mid.style.cssText = `margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;
-          } //3
-          else if(myScore & VeScore >= 4)     {
-              arrContent = 1; 
-              midPoint.style.cssText = `background-color: white;border-radius: 10px;`
-              mid.style.cssText = ` margin-left:${myScore}em; padding-top: ${VeScore}em; ` ;  
-          } //2
-          else  { arrContent = 4; 
-            midPoint.style.cssText = `background-color: white;border-radius: 10px;`
-            mid.style.cssText = ` margin-left:${myScore}em; padding-top: ${VeScore}em;  `;
-          } //1
-         
-      }
-      
-      console.log(arrContent)
-    // myDisplay(myContents[arrContent]) 
-  
-    // console.log(myScore)
-    console.log(VeScore)
-  } 
 
-function analyzer3 (myScore, VeScore, socScore) { 
-  arrContent = 0
-  // if for social score progresist 
-      if(socScore > 13){
-            if (myScore & VeScore  > 15) { 
-              arrContent = 4;
-              progPoint.style.cssText = `background-color: white; border-radius: 10px; `
-              prog.style.cssText = ` padding-left:${myScore}em; padding-top: ${VeScore}em; `;  
-          } //5
-          else if(myScore & VeScore > 10)  {
-              arrContent = 3; 
-              progPoint.style.cssText = `background-color: white; border-radius: 10px; `
-              prog.style.cssText = ` padding-left:${myScore}em; padding-top: ${VeScore}em; `;   
-          } //4
-          else  if(myScore & VeScore >= 6 ) {
-              arrContent = 2; 
-              progPoint.style.cssText = `background-color: white; border-radius: 10px; `
-              prog.style.cssText = ` padding-left:${myScore}em; padding-top: ${VeScore}em; `;   
-          } //3
-          else if(myScore & VeScore >= 4)     {
-              arrContent = 1; 
-              progPoint.style.cssText = `background-color: white; border-radius: 10px; `
-              prog.style.cssText = ` padding-left:${myScore}em; padding-top: ${VeScore}em; `;   
-          } //2
-          else  { arrContent = 4; 
-            progPoint.style.cssText = `background-color: white; border-radius: 10px; `
-            prog.style.cssText = ` padding-left:${myScore}em; padding-top: ${VeScore}em; `;    
-          } //1
-         
-      }
-      
-      console.log(arrContent)
-    // myDisplay(myContents[arrContent]) 
-  
-    
-    
-  } 
 
 
  // this function adds the number of points each answer is worth together 
@@ -154,15 +81,16 @@ function total() {
            socScore = socScore + socialPoints[i];
         } 
 
-        console.log(VeScore, VeScore, socScore);
+        // console.log(VeScore, VeScore, socScore);
       analyzer(myScore, VeScore, socScore);      
-      analyzer2(myScore, VeScore, socScore);   
-      analyzer3(myScore, VeScore, socScore);    
+  
   } 
 
+  console.log(conservador,socialMiddle,progresista)
+  console.log(conservador[0].name)
 
-function myDisplay(myContents) { 
-document.getElementById("result").innerHTML = (myContents);
+function myDisplay(conservador) { 
+document.getElementById("result").innerHTML = (conservador);
 
 } 
 
